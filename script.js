140663d40eb68d2207fd6605749a8fd25d787c72
Felipe Estival <felipemauroestival@gmail.com>
2025-11-06 21:38:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,16 @@
 const numero_nota = document.getElementById('nota_usuario')
 
 function calcular(){
-    
-    nota_necessaria = (18 - Number(numero_nota.value)) / 2;
+    if (numero_nota.value > 10) {
+        alert("NÃO É POSSÍVEL NOTA ACIMA DE 10")
+    }
+    else {
+        nota_necessaria = (18 - Number(numero_nota.value)) / 2;
 
-    const p = document.getElementById('resultado')
-    p.textContent = `A nota para passar é ${nota_necessaria} na G2`
+        const p = document.getElementById('resultado')
+        p.textContent = `A nota necessária para passar é ${nota_necessaria}`
+    }
 }
+
 
 
